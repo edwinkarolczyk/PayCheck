ab2ce99832e20c2bf88afb23416153127b4bdde3
edwinkarolczyk <edwinkarolczyk@gmail.com>
2026-09-10 21:16:27 +0200

    PayCheck 0.5.0: launch complete app

--- run.bat
+++ run.bat
@@ -5,10 +5,10 @@ cd /d "%~dp0"
 where py >nul 2>nul
 if %errorlevel%==0 (
   py -m pip install -r requirements.txt
-  py app_full.py
+  py app_050.py
 ) else (
   python -m pip install -r requirements.txt
-  python app_full.py
+  python app_050.py
 )
 
 endlocal
