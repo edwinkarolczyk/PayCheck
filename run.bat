767fa8a2f380ba8c13f017eff0412fde3661f6ec
edwinkarolczyk <edwinkarolczyk@gmail.com>
2026-09-10 18:07:46 +0200

    PayCheck 0.2.1: launch history-enabled app

--- run.bat
+++ run.bat
@@ -5,10 +5,10 @@ cd /d "%~dp0"
 where py >nul 2>nul
 if %errorlevel%==0 (
   py -m pip install -r requirements.txt
-  py app_budget.py
+  py app_history.py
 ) else (
   python -m pip install -r requirements.txt
-  python app_budget.py
+  python app_history.py
 )
 
 endlocal
