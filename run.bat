a7e0bc0fd94cca069f5b98ceaba6702d42d6b398
edwinkarolczyk <edwinkarolczyk@gmail.com>
2026-09-11 07:33:41 +0200

    PayCheck 0.9.0: launch installment manager

--- run.bat
+++ run.bat
@@ -5,10 +5,10 @@ cd /d "%~dp0"
 where py >nul 2>nul
 if %errorlevel%==0 (
   py -m pip install -r requirements.txt
-  py app_080.py
+  py app_090.py
 ) else (
   python -m pip install -r requirements.txt
-  python app_080.py
+  python app_090.py
 )
 
 endlocal
