bfc371e544ce43080792f480e4a8fea505440cd0
edwinkarolczyk <edwinkarolczyk@gmail.com>
2026-09-11 09:49:19 +0200

    PayCheck 3.4.0: add license public key

---
+++ license_public_340.py
@@ -0,0 +1,8 @@
+from __future__ import annotations
+
+# Publiczny klucz Ed25519 do weryfikacji licencji PayCheck.
+# Klucz prywatny właściciela NIE może trafić do repozytorium ani do EXE.
+PUBLIC_KEY_PEM = b"""-----BEGIN PUBLIC KEY-----
+MCowBQYDK2VwAyEAfqrfZ0XFf+bpK5+sIgRugg1WICsG28gUZCTX5gAU6YE=
+-----END PUBLIC KEY-----
+"""
