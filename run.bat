f061fb0f9dd9480cefe041a856f6a29028dc7084
edwinkarolczyk <edwinkarolczyk@gmail.com>
2026-09-11 06:57:08 +0200

    PayCheck 0.6.0: launch monthly workflow app

--- run.bat
+++ run.bat
@@ -5,10 +5,10 @@ cd /d "%~dp0"
 where py >nul 2>nul
 if %errorlevel%==0 (
   py -m pip install -r requirements.txt
-  py app_050.py
+  py app_060.py
 ) else (
   python -m pip install -r requirements.txt
-  python app_050.py
+  python app_060.py
 )
 
 endlocal
