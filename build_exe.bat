9bb00b218106f90c06934ba59e11cee930e5a879
edwinkarolczyk <edwinkarolczyk@gmail.com>
2026-09-11 08:59:50 +0200

    PayCheck 3.0.0: build household finance app

--- build_exe.bat
+++ build_exe.bat
@@ -38,7 +38,7 @@ if errorlevel 1 goto :error
 
 echo.
 echo Buduje PayCheck.exe...
-%PY_CMD% -m PyInstaller --noconfirm --clean --onefile --windowed --name PayCheck app_210.py
+%PY_CMD% -m PyInstaller --noconfirm --clean --onefile --windowed --name PayCheck app_300.py
 if errorlevel 1 goto :error
 
 echo.
