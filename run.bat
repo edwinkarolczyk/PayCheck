f8ee63e1cb149d56e4af12865fafcea49e3aed66
edwinkarolczyk <edwinkarolczyk@gmail.com>
2026-09-11 08:26:54 +0200

    PayCheck 2.0.0: launch complete budget manager

--- run.bat
+++ run.bat
@@ -5,10 +5,10 @@ cd /d "%~dp0"
 where py >nul 2>nul
 if %errorlevel%==0 (
   py -m pip install -r requirements.txt
-  py app_100.py
+  py app_200.py
 ) else (
   python -m pip install -r requirements.txt
-  python app_100.py
+  python app_200.py
 )
 
 endlocal
