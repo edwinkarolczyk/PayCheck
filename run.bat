df548489e1141bdc82ab0f721f56a26bf212040c
edwinkarolczyk <edwinkarolczyk@gmail.com>
2026-09-11 07:47:11 +0200

    PayCheck 1.0.0: launch home dashboard app

--- run.bat
+++ run.bat
@@ -5,10 +5,10 @@ cd /d "%~dp0"
 where py >nul 2>nul
 if %errorlevel%==0 (
   py -m pip install -r requirements.txt
-  py app_090.py
+  py app_100.py
 ) else (
   python -m pip install -r requirements.txt
-  python app_090.py
+  python app_100.py
 )
 
 endlocal
