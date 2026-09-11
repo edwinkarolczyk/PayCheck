ea9400c28530214aee2f1feff9a20ea90de5ed2d
edwinkarolczyk <edwinkarolczyk@gmail.com>
2026-09-11 09:34:27 +0200

    PayCheck 3.2.0: build daily workflow app

--- build_exe.bat
+++ build_exe.bat
@@ -38,7 +38,7 @@ if errorlevel 1 goto :error
 
 echo.
 echo Buduje PayCheck.exe...
-%PY_CMD% -m PyInstaller --noconfirm --clean --onefile --windowed --name PayCheck app_310.py
+%PY_CMD% -m PyInstaller --noconfirm --clean --onefile --windowed --name PayCheck app_320.py
 if errorlevel 1 goto :error
 
 echo.
